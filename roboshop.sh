4ad5bf0f3e7251eff27e940c0ad25a46e1353065
SRINUNAIK208 <srinaik208@gmail>
2025-12-30 21:50:02 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ Domain_Name="srinunayak.online"
 for instance in ${Instance[@]}
 do
  Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00028162e61c10305 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$Instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $Instance != frontend ]
+    if [ $Instance != "frontend" ]
     then
       IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
