253ba4f6a078a77e79ed818afd0b846acd455954
SRINUNAIK208 <srinaik208@gmail>
2025-12-30 21:44:43 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+Security_ID="sg-00028162e61c10305"
+Instance=("frontend" "catalogue" "payment" "shipping" "user" "mongodb" "sql" "rabbitmq" "redis" "cart" "dispatch")
+Zone_ID="Z0311053ECKFL0X0CB0M"
+Domain_Name="srinunayak.online"
+
+for instance in ${Instance[@]}
+do
+ Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00028162e61c10305 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$Instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $Instance != frontend ]
+    then
+      IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+      IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$Instance IP Address::$IP"
+done
