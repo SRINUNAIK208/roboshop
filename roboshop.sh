86684962c691f7cc5afa887583c06cad7b1f7955
SRINUNAIK208 <srinaik208@gmail>
2025-12-30 21:56:38 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,12 +8,12 @@ Domain_Name="srinunayak.online"
 
 for instance in ${Instance[@]}
 do
- Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00028162e61c10305 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$Instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $Instance != "frontend" ]
+ Instance_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00028162e61c10305 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
     then
       IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
       IP=$(aws ec2 describe-instances --instance-ids $Instance_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
-    echo "$Instance IP Address::$IP"
+    echo "$instance IP Address::$IP"
 done
