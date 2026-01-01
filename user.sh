da3701bce39338e1ba1124693a8e970f6ea39d47
SRINUNAIK208 <srinaik208@gmail>
2026-01-01 23:29:48 +0530

    roboshop

--- user.sh
+++ user.sh
@@ -54,6 +54,7 @@ else
   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
   VALIDATE $? "roboshop user created"
 fi
+rm -rf /app
 
 mkdir -p /app 
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
